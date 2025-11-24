506a6f687986871763a0ecbc6e1e5fb899ce11b4
欧阳欢 <3075901913@qq.com>
2025-11-24 15:34:47 +0800

    docs：梳理每张表的status流转变化

--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MYSQL_HOST = os.environ.get("MYSQL_HOST", "127.0.0.1")
-MYSQL_PORT = os.environ.get("MYSQL_PORT", "3306")
-MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "reward")
+MYSQL_PORT = int(os.environ.get("MYSQL_PORT", "3306"))  # Convert to int
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "reward_test")
 MYSQL_USER = os.environ.get("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "123456")
 
